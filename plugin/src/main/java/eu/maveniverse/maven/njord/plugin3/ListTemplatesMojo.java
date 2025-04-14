1784c476e5360986964a59977f52dfe1f9d814b5
Tamas Cservenak <tamas@cservenak.net>
2025-04-14 14:46:57 +0200

    Add new mojo

---
+++ plugin/src/main/java/eu/maveniverse/maven/njord/plugin3/ListTemplatesMojo.java
@@ -0,0 +1,37 @@
+package eu.maveniverse.maven.njord.plugin3;
+
+import eu.maveniverse.maven.njord.shared.store.ArtifactStoreManager;
+import eu.maveniverse.maven.njord.shared.store.ArtifactStoreTemplate;
+import java.io.IOException;
+import java.util.Collection;
+import org.apache.maven.plugins.annotations.Mojo;
+
+/**
+ * List all existing templates.
+ */
+@Mojo(name = "list-templates", threadSafe = true, requiresProject = false)
+public class ListTemplatesMojo extends NjordMojoSupport {
+    @Override
+    protected void doExecute(ArtifactStoreManager artifactStoreManager) throws IOException {
+        logger.info("List of existing ArtifactStoreTemplate:");
+        Collection<ArtifactStoreTemplate> templates = artifactStoreManager.listTemplates();
+        ArtifactStoreTemplate defaultTemplate = artifactStoreManager.defaultTemplate();
+        for (ArtifactStoreTemplate template : templates) {
+            logger.info("- {} {}", template.name(), template == defaultTemplate ? " (default)" : " ");
+            logger.info("    Prefix: {}", template.prefix());
+            logger.info("    Repository Mode: {}", template.repositoryMode());
+            logger.info("    Allow redeploy: {}", template.allowRedeploy());
+            logger.info(
+                    "    Checksum Factories: {}",
+                    template.checksumAlgorithmFactories().isPresent()
+                            ? template.checksumAlgorithmFactories().orElseThrow()
+                            : "Globally configured");
+            logger.info(
+                    "    Omit checksums for: {}",
+                    template.checksumAlgorithmFactories().isPresent()
+                            ? template.checksumAlgorithmFactories().orElseThrow()
+                            : "Globally configured");
+        }
+        logger.info("Total of {} ArtifactStoreTemplate.", templates.size());
+    }
+}
