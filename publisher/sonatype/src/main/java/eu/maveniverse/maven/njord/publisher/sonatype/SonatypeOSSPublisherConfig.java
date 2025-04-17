eaad6f537221055f6409a0bc3ee7952a809ab71a
Tamas Cservenak <tamas@cservenak.net>
2025-04-17 17:04:56 +0200

    Add OSSRH

---
+++ publisher/sonatype/src/main/java/eu/maveniverse/maven/njord/publisher/sonatype/SonatypeOSSPublisherConfig.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.njord.publisher.sonatype;
+
+import static java.util.Objects.requireNonNull;
+
+import eu.maveniverse.maven.njord.shared.Config;
+
+public final class SonatypeOSSPublisherConfig {
+    public static SonatypeOSSPublisherConfig with(Config config) {
+        requireNonNull(config, "config");
+
+        String releaseRepositoryId = "sonatype-oss";
+        String releaseRepositoryUrl = "https://oss.sonatype.org/service/local/staging/deploy/maven2";
+        String snapshotRepositoryId = "sonatype-oss";
+        String snapshotRepositoryUrl = "https://oss.sonatype.org/content/repositories/snapshots";
+
+        if (config.effectiveProperties().containsKey("njord.publisher.sonatype-oss.releaseRepositoryId")) {
+            releaseRepositoryId = config.effectiveProperties().get("njord.publisher.sonatype-oss.releaseRepositoryId");
+        }
+        if (config.effectiveProperties().containsKey("njord.publisher.sonatype-oss.releaseRepositoryUrl")) {
+            releaseRepositoryUrl =
+                    config.effectiveProperties().get("njord.publisher.sonatype-oss.releaseRepositoryUrl");
+        }
+        if (config.effectiveProperties().containsKey("njord.publisher.sonatype-oss.snapshotRepositoryId")) {
+            snapshotRepositoryId =
+                    config.effectiveProperties().get("njord.publisher.sonatype-oss.snapshotRepositoryId");
+        }
+        if (config.effectiveProperties().containsKey("njord.publisher.sonatype-oss.snapshotRepositoryUrl")) {
+            snapshotRepositoryUrl =
+                    config.effectiveProperties().get("njord.publisher.sonatype-oss.snapshotRepositoryUrl");
+        }
+
+        return new SonatypeOSSPublisherConfig(
+                releaseRepositoryId, releaseRepositoryUrl, snapshotRepositoryId, snapshotRepositoryUrl);
+    }
+
+    public static SonatypeOSSPublisherConfig of(
+            String releaseRepositoryId,
+            String releaseRepositoryUrl,
+            String snapshotRepositoryId,
+            String snapshotRepositoryUrl) {
+        return new SonatypeOSSPublisherConfig(
+                releaseRepositoryId, releaseRepositoryUrl, snapshotRepositoryId, snapshotRepositoryUrl);
+    }
+
+    private final String releaseRepositoryId;
+    private final String releaseRepositoryUrl;
+    private final String snapshotRepositoryId;
+    private final String snapshotRepositoryUrl;
+
+    private SonatypeOSSPublisherConfig(
+            String releaseRepositoryId,
+            String releaseRepositoryUrl,
+            String snapshotRepositoryId,
+            String snapshotRepositoryUrl) {
+        this.releaseRepositoryId = requireNonNull(releaseRepositoryId);
+        this.releaseRepositoryUrl = requireNonNull(releaseRepositoryUrl);
+        this.snapshotRepositoryId = requireNonNull(snapshotRepositoryId);
+        this.snapshotRepositoryUrl = requireNonNull(snapshotRepositoryUrl);
+    }
+
+    public String releaseRepositoryId() {
+        return releaseRepositoryId;
+    }
+
+    public String releaseRepositoryUrl() {
+        return releaseRepositoryUrl;
+    }
+
+    public String snapshotRepositoryId() {
+        return snapshotRepositoryId;
+    }
+
+    public String snapshotRepositoryUrl() {
+        return snapshotRepositoryUrl;
+    }
+}
---
+++ publisher/sonatype/src/main/java/eu/maveniverse/maven/njord/publisher/sonatype/SonatypeOSSPublisherFactory.java
@@ -0,0 +1,57 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.njord.publisher.sonatype;
+
+import static java.util.Objects.requireNonNull;
+
+import eu.maveniverse.maven.njord.shared.Config;
+import eu.maveniverse.maven.njord.shared.SessionConfig;
+import eu.maveniverse.maven.njord.shared.publisher.ArtifactStorePublisherFactory;
+import javax.inject.Inject;
+import javax.inject.Named;
+import javax.inject.Singleton;
+import org.eclipse.aether.RepositorySystem;
+import org.eclipse.aether.repository.RemoteRepository;
+
+@Singleton
+@Named(SonatypeOSSPublisherFactory.NAME)
+public class SonatypeOSSPublisherFactory implements ArtifactStorePublisherFactory {
+    public static final String NAME = "sonatype-oss";
+
+    private final RepositorySystem repositorySystem;
+    private final SonatypeCentralRequirementsFactory centralRequirementsFactory;
+
+    @Inject
+    public SonatypeOSSPublisherFactory(
+            RepositorySystem repositorySystem, SonatypeCentralRequirementsFactory centralRequirementsFactory) {
+        this.repositorySystem = requireNonNull(repositorySystem);
+        this.centralRequirementsFactory = requireNonNull(centralRequirementsFactory);
+    }
+
+    @Override
+    public SonatypeNx2Publisher create(SessionConfig sessionConfig) {
+        SonatypeOSSPublisherConfig ossConfig = SonatypeOSSPublisherConfig.with(sessionConfig.config());
+        RemoteRepository releasesRepository = new RemoteRepository.Builder(
+                        ossConfig.releaseRepositoryId(), "default", ossConfig.releaseRepositoryUrl())
+                .build();
+        RemoteRepository snapshotsRepository = new RemoteRepository.Builder(
+                        ossConfig.snapshotRepositoryId(), "default", ossConfig.snapshotRepositoryUrl())
+                .build();
+
+        return new SonatypeNx2Publisher(
+                sessionConfig,
+                repositorySystem,
+                NAME,
+                "Publishes to Sonatype s01",
+                Config.CENTRAL,
+                snapshotsRepository,
+                releasesRepository,
+                snapshotsRepository,
+                centralRequirementsFactory.create(sessionConfig));
+    }
+}
