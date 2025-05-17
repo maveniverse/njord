2ca571db9b8bb6011f53f38eb719e9fdfe8c176e
Tamas Cservenak <tamas@cservenak.net>
2025-05-17 23:23:36 +0200

    Simplify init
    Make it simple

--- core/src/main/java/eu/maveniverse/maven/njord/shared/NjordUtils.java
+++ core/src/main/java/eu/maveniverse/maven/njord/shared/NjordUtils.java
@@ -23,29 +23,20 @@ public final class NjordUtils {
     private NjordUtils() {}
 
     /**
-     * Performs "eager init" of Njord, will fail if session already exists within this Resolver session.
-     * Returns the newly created {@link Session} instance, never {@code null}.
-     */
-    public static Session init(SessionConfig sessionConfig, Function<SessionConfig, Session> sessionFactory) {
-        requireNonNull(sessionConfig, "sessionConfig");
-        requireNonNull(sessionFactory, "sessionFactory");
-        Session ns = sessionFactory.apply(sessionConfig);
-        setNjordSession(sessionConfig.session(), ns); // explodes if present
-        return ns;
-    }
-
-    /**
      * Performs a "lazy init" of Njord, does not fail if config and session already exists within this Resolver session.
      * Returns the existing or newly created {@link Session} instance, never {@code null}.
      */
-    public static Session lazyInit(SessionConfig sessionConfig, Function<SessionConfig, Session> sessionFactory) {
+    public static synchronized Session lazyInit(
+            SessionConfig sessionConfig, Function<SessionConfig, Session> sessionFactory) {
         requireNonNull(sessionConfig, "remoteRepositories");
         requireNonNull(sessionFactory, "sessionFactory");
-        Optional<Session> ns = mayGetNjordSession(sessionConfig.session());
-        if (ns.isEmpty()) {
-            return init(sessionConfig, sessionFactory);
+        Optional<Session> nso = mayGetNjordSession(sessionConfig.session());
+        if (nso.isEmpty()) {
+            Session ns = sessionFactory.apply(sessionConfig);
+            sessionConfig.session().getData().set(Session.class, ns);
+            return ns;
         } else {
-            return ns.orElseThrow();
+            return nso.orElseThrow();
         }
     }
 
@@ -59,15 +50,5 @@ public final class NjordUtils {
             return Optional.empty();
         }
         return Optional.of(ns);
-    }
-
-    private static void setNjordSession(RepositorySystemSession repositorySystemSession, Session session) {
-        requireNonNull(repositorySystemSession, "repositorySystemSession");
-        requireNonNull(session, "njordSession");
-        Session ns = (Session) repositorySystemSession.getData().get(Session.class);
-        if (ns != null) {
-            throw new IllegalStateException("Njord session already present");
-        }
-        repositorySystemSession.getData().set(Session.class, session);
     }
 }
