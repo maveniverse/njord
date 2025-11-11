09198f1f9b5e61cc35109e0e386fe01f55d194a0
Konrad Windszus <kwin@apache.org>
2025-11-11 19:14:26 +0100

    Only create Njord repository connector for distribution repos
    This closes #209

--- extension3/src/main/java/eu/maveniverse/maven/njord/extension3/NjordRepositoryConnectorFactory.java
+++ extension3/src/main/java/eu/maveniverse/maven/njord/extension3/NjordRepositoryConnectorFactory.java
@@ -11,6 +11,7 @@ import static java.util.Objects.requireNonNull;
 
 import eu.maveniverse.maven.njord.shared.NjordUtils;
 import eu.maveniverse.maven.njord.shared.Session;
+import eu.maveniverse.maven.njord.shared.SessionConfig;
 import eu.maveniverse.maven.njord.shared.impl.J8Utils;
 import eu.maveniverse.maven.njord.shared.store.ArtifactStore;
 import java.util.Map;
@@ -34,7 +35,7 @@ import org.slf4j.LoggerFactory;
 public class NjordRepositoryConnectorFactory implements RepositoryConnectorFactory {
     public static final String NAME = "njord";
 
-    private final Logger logger = LoggerFactory.getLogger(NjordRepositoryConnectorFactory.class);
+    private static final Logger logger = LoggerFactory.getLogger(NjordRepositoryConnectorFactory.class);
     private final Map<String, Provider<RepositoryConnectorFactory>> repositoryConnectorFactories;
 
     @Inject
@@ -54,25 +55,52 @@ public class NjordRepositoryConnectorFactory implements RepositoryConnectorFacto
             throws NoRepositoryConnectorException {
         boolean connectorSkip = ConfigUtils.getBoolean(session, false, NjordUtils.RESOLVER_SESSION_CONNECTOR_SKIP);
         if (!connectorSkip) {
+            // is repository used for deployment or for downloading?
             Optional<Session> nso = NjordUtils.mayGetNjordSession(session);
             if (nso.isPresent()) {
                 Session ns = nso.orElseThrow(J8Utils.OET);
-                String url = ns.artifactPublisherRedirector().getRepositoryUrl(repository);
-                if (url != null && url.startsWith(NAME + ":")) {
-                    RepositoryConnectorFactory basicRepositoryConnectorFactory = requireNonNull(
-                            repositoryConnectorFactories.get("basic").get(),
-                            "No basic repository connector factory found");
-                    ArtifactStore artifactStore = ns.getOrCreateSessionArtifactStore(url.substring(6));
-                    return new NjordRepositoryConnector(
-                            artifactStore,
-                            repository,
-                            basicRepositoryConnectorFactory.newInstance(
-                                    artifactStore.storeRepositorySession(session),
-                                    artifactStore.storeRemoteRepository()));
+                if (isDistributionRepo(repository, ns.config())) {
+                    String url = ns.artifactPublisherRedirector().getRepositoryUrl(repository);
+                    if (url != null && url.startsWith(NAME + ":")) {
+                        RepositoryConnectorFactory basicRepositoryConnectorFactory = requireNonNull(
+                                repositoryConnectorFactories.get("basic").get(),
+                                "No basic repository connector factory found");
+                        ArtifactStore artifactStore = ns.getOrCreateSessionArtifactStore(url.substring(6));
+                        return new NjordRepositoryConnector(
+                                artifactStore,
+                                repository,
+                                basicRepositoryConnectorFactory.newInstance(
+                                        artifactStore.storeRepositorySession(session),
+                                        artifactStore.storeRemoteRepository()));
+                    }
                 }
             }
         }
         throw new NoRepositoryConnectorException(repository);
+    }
+
+    static boolean isDistributionRepo(RemoteRepository repository, SessionConfig config) {
+        if (config.currentProject().isPresent()) {
+            RemoteRepository distributionRepository = config.currentProject()
+                    .orElseThrow(J8Utils.OET)
+                    .distributionManagementRepositories()
+                    .get(config.currentProject().orElseThrow(J8Utils.OET).repositoryMode());
+            if (distributionRepository != null
+                    && !distributionRepository.getId().equals(repository.getId())) {
+                logger.debug(
+                        "Repository {} is not having the id of the project's distribution repository {}",
+                        repository,
+                        distributionRepository);
+                return false;
+            } else {
+                logger.debug(
+                        "Haven't found project's distribution repository for repository mode {}",
+                        config.currentProject().orElseThrow(J8Utils.OET).repositoryMode());
+            }
+        } else {
+            logger.debug("No project available every repository may be a distribution one");
+        }
+        return true;
     }
 
     @Override
