d391994507023e7304a2542f3152b3edf6482850
Konrad Windszus <kwin@apache.org>
2025-11-14 14:18:11 +0100

    Skip publishing of empty artifact store
    This closes #221

--- core/src/main/java/eu/maveniverse/maven/njord/shared/impl/DefaultSession.java
+++ core/src/main/java/eu/maveniverse/maven/njord/shared/impl/DefaultSession.java
@@ -277,8 +277,12 @@ public class DefaultSession extends CloseableConfigSupport<SessionConfig> implem
                     try (ArtifactStore as = internalArtifactStoreManager
                             .selectArtifactStore(storeName)
                             .orElseThrow(J8Utils.OET)) {
-                        p.publish(as);
-                        result.addAndGet(1);
+                        if (!as.artifacts().isEmpty()) {
+                            p.publish(as);
+                            result.addAndGet(1);
+                        } else {
+                            logger.warn("Skipping publishing of empty artifact store {}", storeName);
+                        }
                     }
                 }
             } else {
