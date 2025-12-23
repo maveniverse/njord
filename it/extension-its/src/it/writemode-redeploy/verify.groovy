479e28fe402d64df93f810a600a6324e783b51b8
Tamas Cservenak <tamas@cservenak.net>
2025-12-23 17:46:01 +0100

    Add IT for write-mode

---
+++ it/extension-its/src/it/writemode-redeploy/verify.groovy
@@ -0,0 +1,15 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+File buildLog = new File( basedir, 'build.log' )
+assert buildLog.exists()
+var build = buildLog.text
+
+// Lets make strict assertion
+// Also, consider Maven 3 vs 4 diff: they resolve differently; do not assert counts
+
+assert build.contains('Store writemode-redeploy-00002: does not allow write operations.')
