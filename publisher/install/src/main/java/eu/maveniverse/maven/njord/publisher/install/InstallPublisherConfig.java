6be19e872e14dec98f3b698e4eaf66029aa5a609
Tamas Cservenak <tamas@cservenak.net>
2026-02-18 16:41:34 +0100

    Add target for install publisher (#276)
    That may override session LRM, basically creating local
    repositories.

--- publisher/install/src/main/java/eu/maveniverse/maven/njord/publisher/install/InstallPublisherConfig.java
+++ publisher/install/src/main/java/eu/maveniverse/maven/njord/publisher/install/InstallPublisherConfig.java
@@ -10,23 +10,38 @@ package eu.maveniverse.maven.njord.publisher.install;
 import eu.maveniverse.maven.njord.shared.SessionConfig;
 import eu.maveniverse.maven.njord.shared.impl.NjordRepositoryListener;
 import eu.maveniverse.maven.njord.shared.publisher.PublisherConfigSupport;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.Locale;
+import java.util.Optional;
 import org.eclipse.aether.util.ConfigUtils;
 
 /**
  * Install publisher config.
  */
 public final class InstallPublisherConfig extends PublisherConfigSupport {
+    private final Path target;
     private final NjordRepositoryListener.Mode listenerMode;
 
     public InstallPublisherConfig(SessionConfig sessionConfig) {
         super(InstallPublisherFactory.NAME, sessionConfig);
+
+        String targetString = ConfigUtils.getString(sessionConfig.effectiveProperties(), null, keyNames("target"));
+        if (targetString != null) {
+            this.target = Paths.get(targetString).toAbsolutePath();
+        } else {
+            this.target = null;
+        }
 
         this.listenerMode = NjordRepositoryListener.Mode.valueOf(ConfigUtils.getString(
                         sessionConfig.effectiveProperties(),
                         NjordRepositoryListener.Mode.AGGREGATED.name(),
                         keyNames("listenerMode"))
                 .toUpperCase(Locale.ROOT));
+    }
+
+    public Optional<Path> target() {
+        return Optional.ofNullable(target);
     }
 
     public NjordRepositoryListener.Mode listenerMode() {
