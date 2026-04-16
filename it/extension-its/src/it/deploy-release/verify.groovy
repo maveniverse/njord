3b578c55d69cc10a4838d013423ef73024c72668
Tamas Cservenak <tamas@cservenak.net>
2026-04-16 11:43:38 +0200

    Update deploy-release IT; make it run release and also snapshot publishing
    Also, split distro reposes in two distinct distro repositories.
    Goal is to check whether Njord perform proper distro repository
    selection based on project version (rel/snap).

--- it/extension-its/src/it/deploy-release/verify.groovy
+++ it/extension-its/src/it/deploy-release/verify.groovy
@@ -5,21 +5,46 @@
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-v20.html
  */
-File firstLog = new File( basedir, 'first.log' )
-assert firstLog.exists()
-var first = firstLog.text
+File log1f = new File( basedir, 'log-1.log' )
+assert log1f.exists()
+var log1 = log1f.text
 
-File secondLog = new File( basedir, 'second.log' )
-assert secondLog.exists()
-var second = secondLog.text
+File log2f = new File( basedir, 'log-2.log' )
+assert log2f.exists()
+var log2 = log2f.text
+
+File log3f = new File( basedir, 'log-3.log' )
+assert log3f.exists()
+var log3 = log3f.text
+
+File log4f = new File( basedir, 'log-4.log' )
+assert log4f.exists()
+var log4 = log4f.text
+
+File log5f = new File( basedir, 'log-5.log' )
+assert log5f.exists()
+var log5 = log5f.text
 
 // Lets make strict assertion
 // Also, consider Maven 3 vs 4 diff: they resolve differently; do not assert counts
 
 // first run:
-assert first.contains("[INFO] Njord ${projectVersion} session created")
+assert log1.contains("[INFO] Njord ${projectVersion} session created")
 
 // second run:
-assert second.contains("[INFO] Njord ${projectVersion} session created")
-assert second.contains("[INFO] Publishing deploy-release-00001 ")
-assert new File( basedir, "target/releases").isDirectory()
+assert log2.contains("[INFO] Njord ${projectVersion} session created")
+assert log2.contains("[INFO] Publishing deploy-release-00001 ")
+assert log2.contains("[INFO] Published 8 artifact(s) to deploy-release-service repository")
+assert new File( basedir, "releases-repo").isDirectory()
+
+// third run:
+assert log3.contains("[INFO] Njord ${projectVersion} session created")
+
+// fourth run:
+assert log4.contains("[INFO] Njord ${projectVersion} session created")
+
+// fifth run:
+assert log5.contains("[INFO] Njord ${projectVersion} session created")
+assert log5.contains("[INFO] Publishing deploy-release-00002 ")
+assert log5.contains("[INFO] Published 8 artifact(s) to deploy-snapshot-service repository")
+assert new File( basedir, "snapshots-repo").isDirectory()
