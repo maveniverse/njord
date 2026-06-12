002ec8a9dc5f15cdaa6184439f2e5eddb7291bcf
Piotr Findeisen <piotr.findeisen@gmail.com>
2026-06-12 12:31:11 +0200

    Skip .njord directory creation when njord.enabled=false

--- extension3/src/main/java/eu/maveniverse/maven/njord/extension3/NjordSessionLifecycleParticipant.java
+++ extension3/src/main/java/eu/maveniverse/maven/njord/extension3/NjordSessionLifecycleParticipant.java
@@ -47,6 +47,10 @@ public class NjordSessionLifecycleParticipant extends AbstractMavenLifecyclePart
     public void afterProjectsRead(MavenSession session) throws MavenExecutionException {
         requireNonNull(session);
         try {
+            if (isExplicitlyDisabled(session)) {
+                logger.debug("Njord disabled via properties, skipping session initialization");
+                return;
+            }
             // session config
             SessionConfig sc = SessionConfig.defaults(
                             session.getRepositorySession(),
@@ -119,5 +123,17 @@ public class NjordSessionLifecycleParticipant extends AbstractMavenLifecyclePart
         } catch (IOException e) {
             throw new MavenExecutionException("Error closing Njord", e);
         }
+    }
+
+    private static boolean isExplicitlyDisabled(MavenSession session) {
+        String userProperty = session.getUserProperties().getProperty(SessionConfig.CONFIG_ENABLED);
+        if (userProperty != null) {
+            return !Boolean.parseBoolean(userProperty);
+        }
+        String systemProperty = session.getSystemProperties().getProperty(SessionConfig.CONFIG_ENABLED);
+        if (systemProperty != null) {
+            return !Boolean.parseBoolean(systemProperty);
+        }
+        return false;
     }
 }
