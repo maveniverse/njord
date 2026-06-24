7ae1fcd4e6d9e1068447da9eea3f3bcdb0ada9ae
Konrad Windszus <kwin@apache.org>
2026-06-24 16:30:20 +0200

    Improve logging for NjordSessionLifecycleParticipant
    Always mention "Njord" to clarify who is responsible for emitting.
    Only log result of njord auto publishing.
    
    This closes #315

--- extension3/src/main/java/eu/maveniverse/maven/njord/extension3/NjordSessionLifecycleParticipant.java
+++ extension3/src/main/java/eu/maveniverse/maven/njord/extension3/NjordSessionLifecycleParticipant.java
@@ -88,31 +88,36 @@ public class NjordSessionLifecycleParticipant extends AbstractMavenLifecyclePart
                                 int dropped = njordSession.dropSessionArtifactStores();
                                 if (dropped != 0) {
                                     logger.warn(
-                                            "Auto publish: Session failed; dropped {} stores created in this session",
+                                            "Njord auto publish: Session failed; dropped {} stores created in this session",
                                             dropped);
                                 } else {
-                                    logger.warn("Auto publish: Session failed; no stores created in this session");
+                                    logger.warn(
+                                            "Njord auto publish: Session failed; no stores created in this session");
                                 }
                             } else {
                                 logger.warn(
-                                        "Auto publish: Session failed; stores created in this session, if any, are not dropped");
+                                        "Njord auto publish: Session failed; stores created in this session, if any, are not dropped");
                             }
                         } else {
                             try {
-                                logger.info("Auto publish: Publishing stores created in this session");
                                 int published = njordSession.publishSessionArtifactStores();
                                 if (published != 0) {
-                                    logger.info("Auto publish: Published {} stores created in this session", published);
+                                    logger.info(
+                                            "Njord auto publish: Published {} stores created in this session",
+                                            published);
                                     if (njordSession.config().autoDrop()) {
                                         int dropped = njordSession.dropSessionArtifactStores();
                                         if (dropped != 0) {
-                                            logger.info("Auto publish: Dropped {} auto published stores", dropped);
+                                            logger.info(
+                                                    "Njord auto publish: Dropped {} auto published stores", dropped);
                                         }
                                     } else {
-                                        logger.info("Auto publish: The {} published stores are not dropped", published);
+                                        logger.info(
+                                                "Njord auto publish: The {} published stores are not dropped",
+                                                published);
                                     }
                                 } else {
-                                    logger.info("Auto publish: No stores created in this session");
+                                    logger.info("Njord auto publish: No stores created in this session");
                                 }
                             } catch (ArtifactStorePublisher.PublishFailedException e) {
                                 throw new MavenExecutionException(e.getMessage(), e);
